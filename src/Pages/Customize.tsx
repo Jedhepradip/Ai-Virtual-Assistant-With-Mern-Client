788f186f75a9820e9f567669decee1d643e0eee3
Jedhepradip <pradipjedhe69@gmail.com>
2025-10-24 23:36:27 +0530

    all errro are solve fix it

--- src/Pages/Customize.tsx
+++ src/Pages/Customize.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import image1 from '../assets/image1.png'
 import image2 from '../assets/image2.jpg'
 import image3 from '../assets/authBg.png'
@@ -9,22 +9,53 @@ import image7 from '../assets/image7.jpeg'
 import Cart from '../Component/Cart'
 import { Upload } from 'lucide-react'
 
+const images = [image1, image2, image3, image4, image5, image6, image7]
+
+
 const Customize: React.FC = () => {
 
-    const images = [image1, image2, image3, image4, image5, image6, image7]
+    const [backendImage, setBackendImage] = useState<File | null>(null)
+    const [frontendImage, setFrontendImage] = useState<string | null>(null)
+
+    const inputImageRef = useRef<HTMLInputElement | null>(null)
+    const handleImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0]
+        if (file) {
+            setBackendImage(file)
+            setFrontendImage(URL.createObjectURL(file))
+        }
+    }
+
+    console.log(backendImage);
 
     return (
-        <div className='w-full min-h-screen bg-gradient-to-t from-black to-[#030353] flex justify-center items-center py-10'>
-            <div className='w-[90%] max-w-[1200px] flex justify-center items-center flex-wrap gap-6'>
+        <div className="w-full min-h-screen bg-gradient-to-t from-black to-[#030353] flex flex-col items-center py-10">
+            <h1 className="text-white text-[28px] font-semibold text-center mb-10">
+                Select your <span className="text-blue-400">Assistant Image</span>
+            </h1>
+
+            <div className="w-[90%] max-w-[1200px] flex justify-center flex-wrap gap-6">
                 {images.map((img, index) => (
                     <Cart key={index} image={img} />
                 ))}
 
-                <div className='w-[185px] h-[250px] bg-[#030326] border-2 border-[#0c0c8f] rounded-2xl overflow-hidden
-                        hover:scale-105 transition-transform duration-300 cursor-pointer hover:shadow-lg hover:shadow-blue-500/50'>
-                    {/* <img src={image} className='h-full object-cover' /> */}
-                    <Upload className='w-full h-full text-white p-10' />
+                {frontendImage && <Cart image={frontendImage} />}
+
+                <div
+                    onClick={() => inputImageRef.current?.click()}
+                    className="w-[185px] h-[250px] bg-[#030326] border-2 border-[#0c0c8f] rounded-2xl flex justify-center items-center
+          hover:scale-105 transition-transform duration-300 cursor-pointer hover:shadow-lg hover:shadow-blue-500/50"
+                >
+                    <Upload className="w-16 h-16 text-white" />
                 </div>
+
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={inputImageRef}
+                    hidden
+                    onChange={handleImage}
+                />
             </div>
         </div>
     )
